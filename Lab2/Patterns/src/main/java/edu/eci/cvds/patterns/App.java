f59c3988e2a5896633b66b2f20b4790bafb1d707
adriana marcela castañeda velasquez <adriana.castaneda98@gmail.com>
2020-01-28 19:00:03 -0500

    FINAL

---
+++ Lab2/Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -0,0 +1,16 @@
+package edu.eci.cvds.patterns;
+
+import java.util.Arrays;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	for (int i=0; i<args.length; i++){
+        	System.out.print( args[i] + " ");
+	}
+    }
+}
